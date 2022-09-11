02dfe4a81e27b668d65bb90dd6eacf3020985521
ShadmaanHussain <shdhussain1299@gmail.com>
2022-09-11 17:59:46 +0530

    Made navigation changes and added some animations

--- src/script.js
+++ src/script.js
@@ -1,9 +1,21 @@
-const burgerBtn = document.querySelector('.burger');
-const navLinks = document.querySelector('.nav-links');
-const body = document.querySelector('body');
+const burgerBtn = document.querySelector('.burger'); // burger button
+const navLinks = document.querySelector('.nav-links'); //ul of the navbar
+const body = document.querySelector('body'); // body of the html
 
-burgerBtn.addEventListener('click', () => {
+function toggleNav() {
     navLinks.classList.toggle('nav-active');
     burgerBtn.classList.toggle('burger-active');
     body.classList.toggle('no-scroll');
+}
+
+// Toggle the navbar using the burger button.
+burgerBtn.addEventListener('click', function () {
+    toggleNav();
+})
+
+// Close the navbar and remove no-scroll after the navigation links are clicked.
+navLinks.addEventListener('click', function (e) {
+   if(e.target.tagName === 'A' && window.innerWidth < 650) {
+    toggleNav();
+   }
 })
